82cb0134cdf2dae7b5458ef3997816c204196bcc
youssef-tarek-abdelaziz <youssef.tarek.abdelaziz@gmail.com>
2025-05-02 22:41:01 +0300

    Initial commit

---
+++ src/main/java/spring/practice/elmenus_lite/ElmenusLiteApplication.java
@@ -0,0 +1,13 @@
+package spring.practice.elmenus_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmenusLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElmenusLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/practice/elmenus_lite/ElmenusLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.practice.elmenus_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmenusLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
