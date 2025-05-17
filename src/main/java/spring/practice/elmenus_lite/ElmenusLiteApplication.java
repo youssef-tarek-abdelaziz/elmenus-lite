2a040fa285541eae323c0af376a795b04ab72d38
Yahya Allam <yallam122@gmail.com>
2025-05-17 23:57:50 +0300

    remove unused imports

--- src/main/java/spring/practice/elmenus_lite/ElmenusLiteApplication.java
+++ src/main/java/spring/practice/elmenus_lite/ElmenusLiteApplication.java
@@ -1,11 +1,9 @@
 package spring.practice.elmenus_lite;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import spring.practice.elmenus_lite.repository.CartRepository;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
