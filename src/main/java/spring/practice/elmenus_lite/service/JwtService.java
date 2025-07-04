79597171466dfe92aa2914007f6fef706948b513
abdelrahman3mr <80067683+abdelrahman3mr@users.noreply.github.com>
2025-07-04 18:47:06 +0300

    Fix JWT init

--- src/main/java/spring/practice/elmenus_lite/service/JwtService.java
+++ src/main/java/spring/practice/elmenus_lite/service/JwtService.java
@@ -10,8 +10,8 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    @Value("${spring.jwt.secret}")
-    private String secret;
+//    @Value("${spring.jwt.secret}")
+//    private String secret;
 
     public String generateToken(String email) {
         final long tokenExpiration = 86400; //1 day
@@ -20,7 +20,7 @@ public class JwtService {
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
                 .compact();
     }
 }
